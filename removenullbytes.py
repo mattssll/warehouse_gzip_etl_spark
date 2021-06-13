acdc7ab305381dc83eb0a0f91ed8136cc45b9465
mateusleao <mateus.leao@vectuxanalytics.com>
2021-06-12 19:11:22 -0500

    v0.1 - remove unnecessary file

--- removenullbytes.py
+++
@@ -1,9 +0,0 @@
-import glob
-myFiles = glob.glob("./products2/*")
-
-import csv
-for file in myFiles:
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            row.replace('\0', '')
