9c01e973e1ad28377057a67cdd5e6d5d1480e17c
mateusleao <mateus.leao@vectuxanalytics.com>
2021-06-14 02:45:13 -0500

    v0.2 adding processed metadata

--- processMetadata.py
+++ processMetadata.py
@@ -1,11 +1,13 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import *
 from pyspark.context import SparkContext
 from pyspark.conf import SparkConf
 from baseLogger import configure_logger
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, LongType, DoubleType, ArrayType
+from pyspark.ml.feature import Bucketizer
 
 logger = configure_logger()
-logger.warn("My test info statement")
+logger.warn("Starting to process metadata")
 
 #log4jLogger = sc._jvm.org.apache.log4j
 #LOGGER = log4jLogger.LogManager.getLogger(__name__)
@@ -20,95 +22,70 @@ spark = SparkSession \
     .config("spark.driver.memory", "8g") \
     .getOrCreate()
 
+initialSchema = StructType([ \
+    StructField("asin",StringType(),False), \
+    StructField("categories", ArrayType(ArrayType(StringType())),True), \
+    StructField("title",StringType(),True), \
+    StructField("price",DoubleType(),True), \
+    StructField("imUrl", StringType(), True), \
+    StructField("description", StringType(), True), \
+    StructField('related', StructType([
+             StructField('also_bought', ArrayType(StringType()), True),
+             StructField('also_viewed', ArrayType(StringType()), True),
+             StructField('bought_together', ArrayType(StringType()), True),
+             StructField('buy_after_viewing', ArrayType(StringType()), True),
+             ])), \
+    StructField("brand", StringType(), True), \
+    StructField("salesRank", StringType(), True)
+  ])
 
 def clean_data(path: str) -> DataFrame:
-    df = spark.read.json(path)
-    newDf = df.withColumn('categories', flatten(df.categories))
-    newDf2 = newDf.withColumn("categories", concat_ws(",",col("categories")))
-    #cleanedDf = df.withColumn("categories",concat_ws(",",col("categories"))) # converting field to string
-    return newDf2
+    df = spark.read.json(path, schema=initialSchema, allowSingleQuotes=True,allowBackslashEscapingAnyCharacter=True)
+    df = df.withColumn("categories", flatten(df.categories))
+    df = df.withColumn("categories", concat_ws(",",col("categories")))
+    df = df.withColumn("also_bought", df.related.also_bought).withColumn("also_bought", concat_ws(",",col("also_bought")))
+    df = df.withColumn("also_viewed", df.related.also_viewed).withColumn("also_viewed", concat_ws(",",col("also_viewed")))
+    df = df.withColumn("bought_together", df.related.bought_together).withColumn("bought_together", concat_ws(",",col("bought_together")))
+    df = df.withColumn("buy_after_viewing", df.related.buy_after_viewing).withColumn("buy_after_viewing", concat_ws(",",col("buy_after_viewing")))
+    df = df.withColumn("sales_rank_type", split(col("salesRank"), ':').getItem(0)).withColumn("sales_rank_type", regexp_replace(col("sales_rank_type"),'\{"','')).withColumn("sales_rank_type", regexp_replace(col("sales_rank_type"),'"',''))
+    df = df.withColumn("sales_rank_pos", split(col("salesRank"), ':').getItem(1)).withColumn("sales_rank_pos", regexp_replace(col("sales_rank_pos"),'}',''))
+    df = df.withColumn("sales_rank_pos", split(col("salesRank"), ' ').getItem(1)).withColumn("sales_rank_pos", regexp_replace(col("sales_rank_pos"),'}',''))
 
+    df = df.drop("related")
+    df = df.drop("salesRank")
+    #df = df.dropDuplicates()
+    df = df.na.drop(subset=["asin"])
+    print(df.printSchema())
+    return df
 
 def write_to_csv(df: DataFrame, path: str, compression: str) -> None:
     print("log: starting to write the csvs, this might take some time")
-    df.write.csv(path = path ,sep=",", header=True, lineSep="\n", escape='"', nullValue=None, compression=compression)
+    df.write.csv(path = path ,sep=",", header=False, lineSep="\n", escape='"', nullValue=None, emptyValue='', compression=compression)
     print("log: finished writing csvs")
 
+def create_sparksql_view_n_query(df: DataFrame, view_name:str, query: str) -> None:
+    df.createOrReplaceTempView(view_name)
+    query = spark.sql(query.format(view_name))
+    logger.debug("writing to disk")
+    #query.write.csv(path = "testarquivos" ,sep=",", header=True, lineSep="\n", escape='"', nullValue=None, compression="gzip")
+    logger.info("Printing results from SQL Query")
+    query.show(20,truncate=True)
+
+#9430088
 cleanedDf = clean_data("metadata/small*")
 print(cleanedDf.printSchema())
-#print(cleanedDf.show(truncate=True))
-#write_to_csv(cleanedDf, "/Users/mateus.leao/Documents/mattssll/takeaway/metadata_csv")
-#print("log: the csvs were written successfully")
+print(cleanedDf.show())
+#print(cleanedDf.count())
+#print(type(cleanedDf))
+#print(cleanedDf.count())
 
+#print(cleanedDf.show(150,truncate=True))
+#create_sparksql_view_n_query(df = cleanedDf, view_name="pmetadata", query="SELECT * FROM {} LIMIT 1000000")
 
-
-
-
-
-
-
-
-
-
+#write_to_csv(cleanedDf, "/Users/mateus.leao/Documents/mattssll/takeaway/metadatatest", compression="gzip")
+#logger.info("the csvs were written successfully")
 
 """
-import glob
-import psycopg2
-from multiprocessing import Pool, cpu_count
-
-
-filepath="/base_path/psql_multiprocessing_data"
-
-df.repartition(400) \
-    .write \
-    .mode("overwrite") \
-    .format("csv") \ # even faster using binary format, but ok with csv
-    .save(filepath,header='false')
-
-file_path_list=sorted(glob.glob("/base_path/psql_multiprocessing_data/*.csv"))
-
-def psql_copy_load(fileName):
-    con = psycopg2.connect(database="takeaway",user="my_user",password="admin",host="127.0.0.1",port="5432")
-    cursor = con.cursor()
-    with open(fileName, 'r') as f:
-        # next(f)  # in case to skip the header row.
-        cursor.copy_from(f, 'my_schema.my_table', sep="|")
-
-    con.commit()
-    con.close()
-    return (fileName)
-
-
-with Pool(cpu_count()) as p:
-        p.map(psql_copy_load,file_path_list)
-
-print("parallelism (cores): ",cpu_count())
-print("files processed: ",len(file_path_list))
-
-
-
-
-
-
-
-
-import psycopg2
-#iterate over your files here and generate file object you can also get files list using os module
-file = open('path/to/save/data/part-00000_0.csv')
-file1 = open('path/to/save/data/part-00000_1.csv')
-
-#define a function
-def execute_copy(fileName):
-    con = psycopg2.connect(database=dbname,user=user,password=password,host=host,port=port)
-    cursor = con.cursor()
-    cursor.copy_from(fileName, 'table_name', sep=",")
-    con.commit()
-    con.close()
-
-
-from multiprocessing import Pool, cpu_count
-with Pool(cpu_count()) as p:
-        print(p.map(execute_copy, [file,file1]))
 5:28
 #40 mi rows in 10 mins
 # 20 secs to copy a gb of data - estimate of 15 mins
--- processProductReviews.py
+++ processProductReviews.py
@@ -1,5 +1,9 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import *
+from baseLogger import configure_logger
+
+logger = configure_logger()
+logger.info("starting to process product reviews data")
 
 spark = SparkSession \
     .builder \
@@ -7,29 +11,49 @@ spark = SparkSession \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-def clean_data(path):
+def clean_data(path: str) -> DataFrame:
     """
     This function reads data from multiple json files and clean the data
     so it can be inserted to postgresql later on.
     """
+    logger.debug("reading data from multiple JSONs and cleaning it")
     df = spark.read.json(path)
-    fixTypedf = df.withColumn("helpful", df["helpful"].cast("string"))
+    df = df.withColumn("reviewID", monotonically_increasing_id())
+    df = df.withColumn("helpful", df["helpful"].cast("string")).withColumn("overall", df["overall"].cast("integer"))
+    df= df.withColumn("reviewTime", to_date(df.reviewTime, 'yyyy-MM-dd'))
     null = u'\u0000'
     colsToClean = ['reviewText','reviewerID', 'summary']
-    fixNullDf = fixTypedf.select(
+    df = df.select(
           *(regexp_replace(col(c), null, '').alias(c) if c in colsToClean else c for
             c in df.columns)
       )
-    return fixNullDf
+    logger.debug("DataFrame was successfully cleaned")
+    df = df.dropDuplicates()
+    return df
 
-def write_to_csv(df, path, compression):
+def create_sparksql_view_n_query(df: DataFrame, view_name:str, query: str) -> None:
+    df.createOrReplaceTempView(view_name)
+    query = spark.sql(query.format(view_name))
+    print(query)
+    logger.debug("writing to disk")
+
+def write_to_csv(df: DataFrame, path: str, compression: str) -> None:
+    logger.debug("starting to write dataframe to multiple CSVs")
     print("log: starting to write the csvs, this might take some time")
-    df.write.csv(path = path ,sep=",", header=True, lineSep="\n", escape='"', nullValue=None, compression=compression)
-    print("log: finished writing csvs")
+    df.write.csv(path = path ,sep=",", header=False, lineSep="\n", escape='"', nullValue=None, compression=compression)
+    logger.debug("Data was successfully written to CSVs")
+
+
 
 cleanedDf = clean_data("products/smaller*")
-write_to_csv(cleanedDf, "/Users/mateus.leao/Documents/mattssll/takeaway/json_split", "gzip")
+#create_sparksql_view_n_query(df = cleanedDf, view_name="productsReview", query="SELECT count(distinct reviewerid) FROM {}")
+
+
+
+#write_to_csv(cleanedDf, "/Users/mateus.leao/Documents/mattssll/takeaway/json_split", "gzip")
 print("log: the csvs were written successfully")
+print(cleanedDf.printSchema())
+print(cleanedDf.show())
 #changedTypedf.printSchema()
 #df = spark.read.csv("/Users/mateus.leao/Documents/mattssll/takeaway/json_split/part*.csv")
 #print(df.count())
@@ -42,70 +66,3 @@ print("log: the csvs were written successfully")
 
 
 
-
-
-
-
-"""
-import glob
-import psycopg2
-from multiprocessing import Pool, cpu_count
-
-
-filepath="/base_path/psql_multiprocessing_data"
-
-df.repartition(400) \
-    .write \
-    .mode("overwrite") \
-    .format("csv") \ # even faster using binary format, but ok with csv
-    .save(filepath,header='false')
-
-file_path_list=sorted(glob.glob("/base_path/psql_multiprocessing_data/*.csv"))
-
-def psql_copy_load(fileName):
-    con = psycopg2.connect(database="takeaway",user="my_user",password="admin",host="127.0.0.1",port="5432")
-    cursor = con.cursor()
-    with open(fileName, 'r') as f:
-        # next(f)  # in case to skip the header row.
-        cursor.copy_from(f, 'my_schema.my_table', sep="|")
-
-    con.commit()
-    con.close()
-    return (fileName)
-
-
-with Pool(cpu_count()) as p:
-        p.map(psql_copy_load,file_path_list)
-
-print("parallelism (cores): ",cpu_count())
-print("files processed: ",len(file_path_list))
-
-
-
-
-
-
-
-
-import psycopg2
-#iterate over your files here and generate file object you can also get files list using os module
-file = open('path/to/save/data/part-00000_0.csv')
-file1 = open('path/to/save/data/part-00000_1.csv')
-
-#define a function
-def execute_copy(fileName):
-    con = psycopg2.connect(database=dbname,user=user,password=password,host=host,port=port)
-    cursor = con.cursor()
-    cursor.copy_from(fileName, 'table_name', sep=",")
-    con.commit()
-    con.close()
-
-
-from multiprocessing import Pool, cpu_count
-with Pool(cpu_count()) as p:
-        print(p.map(execute_copy, [file,file1]))
-5:28
-#40 mi rows in 10 mins
-# 20 secs to copy a gb of data - estimate of 15 mins
-# 5 mins to copy my JSON to CSVS - damn fast!
-"""
